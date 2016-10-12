2b741343983ac2a89f611bd4fc64f2c755ad3481
Brian <brian.houng@gmail.com>
2016-10-11 21:05:33 -0700

    add custom filter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,28 @@
  * Controller of the productListApp
  */
 angular.module('productListApp')
-  .controller('MainCtrl', function ($scope, globalScope) {
+  .controller('MainCtrl', function ($scope, globalScope, $timeout) {
   	$scope.quantity = 9;
     globalScope.getProducts().then(function(data){
     	$scope.products = data.data.products;
     });
-    $scope.checkFilterLength = function(val){
+    $scope.doFilter = function(val){
     	$scope.quantity = $scope.changeLength(val);
+        $timeout.cancel($scope.timer);
+        $scope.timer = $timeout(function(){
+            globalScope.getProducts().then(function(data){
+                $scope.products = data.data.products;
+                $scope.products = $scope.products.filter(function(item){
+                    var result = [];
+                    for(var key in item){
+                        var k = item[key].toLowerCase();
+                        if(k.indexOf(val.toLowerCase()) > -1){
+                            return item;
+                        }
+                    }
+                });
+            });
+        }, 500);
     };
 
     $scope.clearFilter = function(){
